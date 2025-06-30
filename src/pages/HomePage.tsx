14125cba4105a31b71f8eeb5d1d827a42354d534
qowldud <bjy6631@daum.net>
2025-07-01 02:37:55 +0900

    feat: navbar ui 구현

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,10 @@
+import { Navbar } from "../components/navbar/Navbar";
+
 export const HomePage = () => {
-  return <div className="header-h4">Home Page 입니다.</div>;
+  return (
+    <div className="header-h4">
+      Home Page 입니다.
+      <Navbar />
+    </div>
+  );
 };
