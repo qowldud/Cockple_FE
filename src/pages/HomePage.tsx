a81034f19f51c37a5e1d0f2d7042c36c5dce67f8
qowldud <bjy6631@daum.net>
2025-07-01 23:44:40 +0900

    feat: kakao social Login 버튼 ui 구현

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,14 +2,16 @@ import { Footer } from "../components/common/Footer";
 import { MainHeader } from "../components/common/header/MainHeader";
 import { PageHeader } from "../components/common/header/PageHeader";
 import { Navbar } from "../components/common/navbar/Navbar";
+import { SocialLoginBtn } from "../components/common/SocialLoginBtn";
 
 export const HomePage = () => {
   return (
-    <div className="header-h4">
+    <div className="flex flex-col justify-center items-center  header-h4">
       Home Page 입니다.
       <MainHeader hasNotification={true} />
       <PageHeader title="Pages" />
       <Footer />
+      <SocialLoginBtn />
       <Navbar />
     </div>
   );
