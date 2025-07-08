3c2196634ea7841d10eb7722fd43979d45ed8292
JeonSuna <jsa020910@gmail.com>
2025-07-08 09:55:58 +0900

    test: .

--- src/pages/login/LoginSwiping.tsx
+++ src/pages/login/LoginSwiping.tsx
@@ -12,19 +12,21 @@ export default function LoginSwiping({
   text2,
 }: LoginProps) {
   return (
-    <div className="flex items-center flex-col  justify-center w-full">
-      <div className="mb-10 header-h3 pt-14 h-17 ">
+    <div className="flex items-center flex-col  justify-center w-full ">
+      <div className="mb-10 header-h3 pt-20 ">
         <p>{title} </p>
-        <p>{title2} </p>
+        {title2 && <p>{title2}</p>}
       </div>
-      <div className=" mb-10  mt-10">
+
+      <div className={`mb-10 ${title2 ? "-mt-5" : "mb-1"}`}>
         <img
           src="/src/assets/images/image.png"
           alt="캐릭터img"
-          className="size-50  aspect-square"
+          className="size-50  aspect-square object-cover"
         />
       </div>
-      <div className="header-h5 mb-12 max-w-60 pb-10">
+
+      <div className="header-h5 mb-9 max-w-60 ">
         <p>{text1}</p>
         <p>{text2}</p>
       </div>
