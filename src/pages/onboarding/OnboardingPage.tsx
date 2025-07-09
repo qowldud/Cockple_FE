8d4dc759b2cdb6b7c71d1958b068e4bd624de68f
JeonSuna <jsa020910@gmail.com>
2025-07-09 14:12:34 +0900

    feat: onBoarding 완성

--- src/pages/onboarding/OnboardingPage.tsx
+++ src/pages/onboarding/OnboardingPage.tsx
@@ -1,12 +1,12 @@
-import Btn_Static from "../../components/common/Btn_Static/Btn_Static";
+import Grad_GR400_L from "../../components/common/Btn_Static/Text/Grad_GR400_L";
 
 export const OnboardingPage = () => {
   return (
     <>
-      <div className=" justify-center w-full min-h-screen flex flex-col">
-        <div className="flex items-center flex-col gap-25">
+      <div className=" w-full min-h-screen flex flex-col">
+        <div className="flex items-center flex-col gap-25 grow">
           <div className="flex flex-col gap-2  items-stretch text-left w-full">
-            <p className="header-h4 pt-20">환영합니다!</p>
+            <p className="header-h4 pt-15">환영합니다!</p>
             <div className="body-md-500">
               <p>카카오계정으로 가입되었어요</p>
               <p>회원 정보를 입력하고 콕플을 즐겨볼까요?</p>
@@ -21,8 +21,8 @@ export const OnboardingPage = () => {
             <p className="header-h4 mb-20">닉네임</p>
           </div>
         </div>
-        <div className="flex items-center justify-center">
-          <Btn_Static kind="GR400" size="L" label="sfds" />
+        <div className="flex items-center justify-center header-h4 ">
+          <Grad_GR400_L label="회원 정보 입력" />
         </div>
       </div>
     </>
