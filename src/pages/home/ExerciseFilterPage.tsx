c0491360e88c4a2669968348fed239eb62eb2598
qowldud <bjy6631@daum.net>
2025-07-18 00:05:38 +0900

    refactor: Toggle 기능 공통 컴포넌트 분리

--- src/pages/home/ExerciseFilterPage.tsx
+++ src/pages/home/ExerciseFilterPage.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { PageHeader } from "../../components/common/system/header/PageHeader";
-import { Accordion } from "../../components/home/Accordion";
 import { MultiSelectButtonGroup } from "../../components/home/MultiSelectButtonGroup";
 import {
   isFilterDirty,
@@ -9,6 +8,7 @@ import {
 import { useNavigate } from "react-router-dom";
 import { CautionModal } from "./CautionModal";
 import { CustomSelect } from "./CustomSelect";
+import { Toggle } from "../../components/common/Toggle";
 
 const cities = [
   { label: "서울특별시", value: "서울특별시", enabled: true },
@@ -81,7 +81,7 @@ export const ExerciseFilterPage = () => {
         <PageHeader title="필터" onBackClick={handleBack} />
 
         <div className="flex flex-col gap-vertical-section-s">
-          <Accordion title="지역">
+          <Toggle title="지역">
             <div className="flex justify-between">
               <CustomSelect
                 options={cities}
@@ -109,8 +109,8 @@ export const ExerciseFilterPage = () => {
                 placeholder="전체"
               />
             </div>
-          </Accordion>
-          <Accordion title="전국 급수">
+          </Toggle>
+          <Toggle title="전국 급수">
             <MultiSelectButtonGroup
               options={[
                 "전체",
@@ -126,23 +126,23 @@ export const ExerciseFilterPage = () => {
               selected={level}
               onChange={newVal => setFilter("level", newVal)}
             />
-          </Accordion>
-          <Accordion title="운동 스타일">
+          </Toggle>
+          <Toggle title="운동 스타일">
             <MultiSelectButtonGroup
               options={["전체", "여복", "남복", "혼복"]}
               selected={style}
               singleSelect={true}
               onChange={newVal => setFilter("style", newVal)}
             />
-          </Accordion>
-          <Accordion title="활동 시간">
+          </Toggle>
+          <Toggle title="활동 시간">
             <MultiSelectButtonGroup
               options={["상시", "오전", "오후"]}
               selected={time}
               singleSelect={true}
               onChange={newVal => setFilter("time", newVal)}
             />
-          </Accordion>
+          </Toggle>
         </div>
       </div>
 
