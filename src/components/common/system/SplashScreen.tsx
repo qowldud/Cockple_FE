14cfe0f51f3564a9a4be251ce2b12c01fdf47fd8
JeonSuna <jsa020910@gmail.com>
2025-07-19 14:38:30 +0900

    feat: splash dvh로 변경

--- src/components/common/system/SplashScreen.tsx
+++ src/components/common/system/SplashScreen.tsx
@@ -1,6 +1,9 @@
 export default function SplashScreen() {
   return (
-    <div className="min-h-screen  w-full flex items-center justify-center bg-white animate-fade-out">
+    <div
+      className=" w-full flex items-center justify-center bg-white animate-fade-out"
+      style={{ height: "100dvh" }}
+    >
       <img
         src="/src/assets/images/Logo_Typo.png"
         className="px-6"
