9c17878dcf54503a2876195681318a3f3e35d816
qowldud <bjy6631@daum.net>
2025-08-15 14:31:10 +0000

    fix 오류 수정

--- src/layout/PrivateRoute.tsx
+++ src/layout/PrivateRoute.tsx
@@ -1,19 +1,17 @@
-import { useEffect } from "react";
 import { ScrollToTop } from "../components/common/ScrollToTop";
-import { useNavigate } from "react-router-dom";
 
 interface PrivateRouteProps {
   children?: React.ReactNode;
 }
 
 export const PrivateRoute = ({ children }: PrivateRouteProps) => {
-  const navigate = useNavigate();
-  const isAuthenticated = localStorage.getItem("accessToken");
-  useEffect(() => {
-    if (!isAuthenticated) {
-      navigate("/login");
-    }
-  }, [isAuthenticated, navigate]);
+  // const navigate = useNavigate();
+  // const isAuthenticated = localStorage.getItem("accessToken");
+  // useEffect(() => {
+  //   if (!isAuthenticated) {
+  //     navigate("/login");
+  //   }
+  // }, [isAuthenticated, navigate]);
   return (
     <>
       <ScrollToTop />
