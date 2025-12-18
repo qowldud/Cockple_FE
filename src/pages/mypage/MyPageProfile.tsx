a75b5a6b31b367485ad0a579b069933c44062a3a
qowldud <bjy6631@daum.net>
2025-12-18 02:34:04 +0000

    chore: 개인채팅 보내기 기능 주석 제거

--- src/pages/mypage/MyPageProfile.tsx
+++ src/pages/mypage/MyPageProfile.tsx
@@ -5,17 +5,18 @@ import { PageHeader } from "../../components/common/system/header/PageHeader";
 import { useNavigate, useParams } from "react-router-dom";
 import { MyPage_Profile_Medal } from "../../components/common/contentcard/MyPage_Profile_Medal";
 import { getProfile } from "../../api/member/profile";
-import { getOtherUserMedals } from "../../api/contest/member"; 
+import { getOtherUserMedals } from "../../api/contest/member";
 import type { ProfileResponseData } from "../../api/member/profile";
 import { useState, useEffect } from "react";
-// import { createDirectChat } from "../../api/chat/direct";
+import { createDirectChat } from "../../api/chat/direct";
 export const MyPageProfile = () => {
-
   const { memberId } = useParams<{ memberId: string }>();
   const numericMemberId = memberId ? Number(memberId) : null;
   const navigate = useNavigate();
 
-  const [profileData, setProfileData] = useState<ProfileResponseData | null>(null);
+  const [profileData, setProfileData] = useState<ProfileResponseData | null>(
+    null,
+  );
   const [medalData, setMedalData] = useState({
     myMedalTotal: 0,
     goldCount: 0,
@@ -54,7 +55,6 @@ export const MyPageProfile = () => {
           silverCount: medals.silverCount,
           bronzeCount: medals.bronzeCount,
         });
-
       } catch (e: any) {
         setError(e.message || "데이터를 불러오는 중 오류가 발생했습니다.");
       } finally {
@@ -64,8 +64,8 @@ export const MyPageProfile = () => {
 
     fetchData();
   }, [numericMemberId]);
-  
-  // 급수 
+
+  // 급수
   function convertLevel(level: string): string {
     const levelMap: Record<string, string> = {
       EXPERT: "자강",
@@ -80,24 +80,24 @@ export const MyPageProfile = () => {
     };
     return level ? (levelMap[level.toUpperCase()] ?? "급수 없음") : "급수 없음";
   }
-//   const handleChatClick = async () => {
-//   if (!numericMemberId) return;
+  const handleChatClick = async () => {
+    if (!numericMemberId) return;
 
-//   try {
-//     const res = await createDirectChat(numericMemberId);
-//     if (res.success && res.data?.chatRoomId) {
-//       navigate(`/chat/personal/${res.data.chatRoomId}`);
-//     } else {
-//       console.error("채팅방 생성 실패", res);
-//     }
-//   } catch (err: any) {
-//     console.error(err);
-//   }
-// };
-
+    try {
+      const res = await createDirectChat(numericMemberId);
+      if (res.success && res.data?.chatRoomId) {
+        navigate(`/chat/personal/${res.data.chatRoomId}`);
+      } else {
+        console.error("채팅방 생성 실패", res);
+      }
+    } catch (err: any) {
+      console.error(err);
+    }
+  };
 
   if (loading) return <div className="text-center py-10">로딩 중...</div>;
-  if (error) return <div className="text-center py-10 text-red-500">에러: {error}</div>;
+  if (error)
+    return <div className="text-center py-10 text-red-500">에러: {error}</div>;
 
   return (
     <div className="flex flex-col overflow-hidden w-full">
@@ -105,23 +105,22 @@ export const MyPageProfile = () => {
       <Profile
         name={profileData?.memberName || ""}
         gender={profileData?.gender === "MALE" ? "MALE" : "FEMALE"}
-        level={convertLevel(profileData?.level || "")} 
+        level={convertLevel(profileData?.level || "")}
         birth={profileData?.birth || ""}
         profileImage={profileData?.profileImgUrl || ""}
       />
-
 
       <div className="my-8 flex flex-col gap-4 w-full items-center">
         <MyPage_Text
           textLabel="모임"
           numberValue={profileData?.myGroupCount ?? 0}
-          onClick={() => 
+          onClick={() =>
             navigate(`/mypage/mygroup?memberId=${numericMemberId}`)
           }
           // onClick={() => navigate("/mypage/profile/group")}
         />
 
-       <MyPage_Profile_Medal
+        <MyPage_Profile_Medal
           myMedalTotal={medalData.myMedalTotal}
           goldCount={medalData.goldCount}
           silverCount={medalData.silverCount}
@@ -139,10 +138,7 @@ export const MyPageProfile = () => {
         />
       </div>
 
-      <Grad_GR400_L
-        label="개인 채팅 보내기"
-        // onClick={handleChatClick}
-      />
+      <Grad_GR400_L label="개인 채팅 보내기" onClick={handleChatClick} />
     </div>
   );
 };
